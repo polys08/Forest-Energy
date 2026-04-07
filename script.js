8580a6f5868aa6d4be0ac6bb58439a44bd136177
polys08 <polyana.lima@grupointegrado.br>
2026-04-06 22:21:56 -0300

    Add files via upload

--- script.js
+++ script.js
@@ -1,5 +1,3 @@
-<script src="js/script.js"></script>
-
 // Pega os elementos do DOM
 const hamburger = document.getElementById('hamburger'); // botão hamburger
 const nav = document.getElementById('nav'); // menu de navegação
@@ -7,7 +5,7 @@ const nav = document.getElementById('nav'); // menu de navegação
 // Quando o usuário clicar no hamburger
 hamburger.addEventListener('click', () => {
     // Alterna a classe 'open' no menu para mostrar/esconder
-    nav.classList.toggle('open');
+    nav.classList.toggle('active');
 
     // Alterna a classe 'active' no hamburger para animar as linhas
     hamburger.classList.toggle('active');
